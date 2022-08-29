0f55de9f156ab3d37f4a2872d4ed9b1423a1acfd
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-08-29 14:28:45 +0300

    commit tree 8638dfef6697a6666b51f726c4603af6315feefe

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Mr. Chocolate', 12, 'M', 'BLACK', 'LOYALTY', 'TRUE');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Rowdy', 15, 'M', 'BLACK', 'PLAYFUL', 'FALSE');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Tabitha', 10, 'F', 'BLACK', 'LOVING', 'TRUE');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Sergeant Brown', 8, 'M', 'BLACK', 'DANGEROUS', 'TRUE');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Melissa', 19, 'F', 'BLACK', 'VIGILANT', 'TRUE');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Grinch', 11, 'M', 'BLACK', 'FURIOUS', 'FALSE');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Wendy', 5, 'F', 'BROWN', 'CARING', 'TRUE');
+INSERT INTO bears (name) VALUES (NULL);
